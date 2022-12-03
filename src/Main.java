670a9def0f2e9ab4816362612ce00697f135f456
Oleg <olegmedvedev7@mail.ru>
2022-12-03 22:27:07 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -8,13 +8,12 @@ public class Main {
         int bonus;
         if (depositedMoney > 1000) {
             bonus = (int) depositedMoney / 100;
-        }
-        else {
+        } else {
             bonus = 0;
         }
         int finalBalance = depositedMoney + balance;
 
-        System.out.println("Бонусов:"+ bonus);
+        System.out.println("Бонусов:" + bonus);
         System.out.println("Ваш баланс:" + finalBalance);
     }
 }
